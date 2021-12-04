36bda450371022d52b5a1fdff2160a1b265dc18a
IrisAmrein <elvira_simi@hotmail.com>
2021-12-04 19:08:32 +0100

    added test for invalid location

--- classes/test_sample.py
+++ classes/test_sample.py
@@ -6,11 +6,12 @@ class TestScrape(unittest.TestCase):
     def setUp(self):
         self.scraper = MeteoScraper(True)
 
-    # def test_weathertext(self):
-    #     """
-    #     tests the main function 
-    #     initiates a new MeteoScraper before every test because otherwise there will be a timeout. (time.sleep did not help)
-    #     """
+    def test_weathertext(self):
+        """
+        tests the main function
+        initiates a new MeteoScraper before every test because otherwise there will be a timeout. (time.sleep did not help)
+        """
+    #     Die resultierenden Texte müssen je nach Datum an dem getestet wird erst angepasst werden bevor getestet werden kann
     #     self.scraper = MeteoScraper(True)
     #     self.assertEqual(self.scraper.find_weather('6005', 0),
     #     "In Luzern ist das Wetter heute Sonne und Wolken im Wechsel. Tagsüber wird es 5 Grad. Nachts wird es 4 Grad.") #dieser Test hat geholfen zu identifizieren dass die Temperaturen verkehr gespeichert wurden!
@@ -19,7 +20,7 @@ class TestScrape(unittest.TestCase):
     #     self.scraper = MeteoScraper(True)
     #     self.assertEqual(self.scraper.find_weather('6005', 2),"In Luzern ist das Wetter am Donnerstag Sonne und Nebelbänke. Tagsüber wird es 3 Grad. Nachts wird es 1 Grad.")
 
-    # def test_daytext(self):
+    def test_daytext(self):
         self.assertEqual(self.scraper.get_reldate(0), "heute")
         self.assertEqual(self.scraper.get_reldate(1), "morgen")
     #     self.assertEqual(self.scraper.get_reldate(2), "am Donnerstag")#Muss je nach Zeitpunkt des Tests angepasst werden. Hier wurde am Dienstag getestet
@@ -30,9 +31,12 @@ class TestScrape(unittest.TestCase):
         self.scraper.driver.get("https://www.srf.ch/meteo/wetter/Dufourspitze/45.9369,7.8668")
         self.assertEqual(self.scraper.get_location_name(), "Dufourspitze")
 
-    # test find_weather for a PLZ that does not exist
-    # test error handling if element not loaded "find element by class name"
-    # test get_day_element & get_temparature with disclaimer that assert Value must be changed depending on day
+    # test that the script ends if an invalid location is called
+    def test_invalid_loc(self):
+        with self.assertRaises(SystemExit) as cm:
+            self.scraper.driver.get("https://www.srf.ch/meteo")
+            self.scraper.nav_to_loc_meteo_page('80005')
+            self.assertEqual(cm.exception,"")
 
     def tearDown(self):
         self.scraper.driver.quit()
